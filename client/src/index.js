5182bfd5330190da6af2d995cd637d1fa9d3e48a
Josh Bryson <josh@bryson.me.uk>
2019-06-04 21:51:34 +0100

    Initial restucturing of client files

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import App from './pages/App';
+import Splash from './pages/Splash';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/login" component={Splash} />
+            </Switch>
+        </Router>
+    );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
