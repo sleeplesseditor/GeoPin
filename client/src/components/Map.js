c423d01f13ac94a4d0a05682cf8a7a106671ff96
Josh Bryson <josh@bryson.me.uk>
2019-06-15 19:12:14 +0100

    Created pins displayed on Map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -5,6 +5,8 @@ import { mapBoxApiId } from './config';
 // import Button from '@material-ui/core/Button';
 // import Typography from '@material-ui/core/Typography';
 // import DeleteIcon from '@material-ui/icons/DeleteTwoTone';
+import { useClient } from '../client';
+import { GET_PINS_QUERY } from '../graphql/queries';
 import PinIcon from './PinIcon';
 import Blog from './Blog';
 import Context from '../context';
@@ -16,7 +18,11 @@ const INITIAL_VIEWPORT = {
 }
 
 const Map = ({ classes }) => {
+    const client = useClient();
     const { state, dispatch } = useContext(Context);
+    useEffect(() => {
+        getPins()
+    }, []);
     const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
     const [userPosition, setUserPosition] = useState(null);
     useEffect(() => {
@@ -31,6 +37,14 @@ const Map = ({ classes }) => {
                 setUserPosition({ latitude, longitude })
             })
         }
+    }
+
+    const getPins = async () => {
+        const { getPins } = await client.request(GET_PINS_QUERY)
+        dispatch({ 
+            type: "GET_PINS",
+            payload: getPins 
+        })
     }
 
     const handleMapClick = ({ lngLat, leftButton }) => {
@@ -88,6 +102,21 @@ const Map = ({ classes }) => {
                         />
                     </Marker>
                 )}
+
+                {state.pins.map(pin => (
+                    <Marker
+                        key={pin._id}
+                        latitude={pin.latitude}
+                        longitude={pin.longitude}
+                        offsetLeft={-19}
+                        offsetTop={-37}
+                    >
+                    <PinIcon 
+                        size={40}
+                        color="darkblue"
+                    />
+                    </Marker>
+                ))}
             </ReactMapGL>
             <Blog />
         </div>
