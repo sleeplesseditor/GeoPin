7263169ab7714e21c2f2ff402ecc9d77e8093e1b
Josh Bryson <josh@bryson.me.uk>
2019-06-16 01:36:54 +0100

    Pin popup functionality added

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -19,6 +19,7 @@ export default function reducer(state, action) {
         case "CREATE_DRAFT":
             return {
                 ...state,
+                currentPin: null,
                 draft: {
                     latitude: 0,
                     longitude: 0
@@ -46,6 +47,12 @@ export default function reducer(state, action) {
                 ...state,
                 pins: [...prevPins, newPin]
             };
+        case "SET_PIN":
+            return {
+                ...state,
+                currentPin: action.payload,
+                draft: null
+            }
         default:
             return state;
     }
